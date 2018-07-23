d7c71335f3e66e737ccb8969d24e252b44d28d0e
Emanuel Mares <emanuel.mares@gmail.com>
2018-07-23 22:49:00 +0300

    Added review styling

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -150,14 +150,17 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  name.classList.add('review-name');
   li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
+  date.classList.add('review-date');
   li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.classList.add('review-rating');
   li.appendChild(rating);
 
   const comments = document.createElement('p');
